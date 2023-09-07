f83961139e58ef1bb711952ba3ca84cdeb69db7b
장관영 <jkypch@adcapsule.co.kr>
2023-09-07 17:42:16 +0900

    first commit

---
+++ src/main/java/com/study0907/study0907/Study0907Application.java
@@ -0,0 +1,13 @@
+package com.study0907.study0907;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0907Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study0907Application.class, args);
+	}
+
+}
